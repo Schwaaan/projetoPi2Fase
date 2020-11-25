5041352735734420d6cb1d6f0b252344c3b75c19
Jair Schwan <jair.schwan9@gmail.com>
2020-11-24 21:46:15 -0300

    last version be

---
+++ src/BE/utils/ValidateTextArea.java
@@ -0,0 +1,13 @@
+package BE.utils;
+
+import javax.swing.JComponent;
+import javax.swing.JTextArea;
+
+public class ValidateTextArea extends Validation {
+
+  @Override
+  public boolean validate(JComponent jComponents) {
+    JTextArea jTextArea = (JTextArea) jComponents;
+    return !jTextArea.getText().isBlank();
+  }
+}
---
+++ src/BE/utils/Validation.java
@@ -0,0 +1,9 @@
+package BE.utils;
+
+import javax.swing.JComponent;
+
+public abstract class Validation {
+
+  public abstract boolean validate(JComponent jComponents);
+
+}
